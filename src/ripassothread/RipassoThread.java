18e48aa7bc9be6cf08eb075c935983c4dc44d69b
lai2 <informatica@lai2>
2019-10-09 11:59:26 +0200

    Added sync logic for class Printer

--- src/ripassothread/RipassoThread.java
+++ src/ripassothread/RipassoThread.java
@@ -1,12 +1,16 @@
 package ripassothread;
 
+import java.util.concurrent.Semaphore;
+
 public class RipassoThread
 {
     public static void main(String[] args)
     {
+        Semaphore lettore =new Semaphore(1);
+        Semaphore scrittore =new Semaphore(0);
+        
         Storage s = new Storage();
-        Counter c = new Counter("c1", s);
-        Printer p = new Printer("p1", s);
+        Printer p = new Printer("p1", s,lettore,scrittore);
     }
     
 }
