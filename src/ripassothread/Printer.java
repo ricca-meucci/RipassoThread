3abfdb81f4150c13efd931669d19c95221147e02
lai2 <informatica@lai2>
2019-10-09 11:31:12 +0200

    Added class Printer and Storage

---
+++ src/ripassothread/Printer.java
@@ -0,0 +1,20 @@
+package ripassothread;
+
+public class Printer implements Runnable
+{
+    private Thread thr;
+    private Storage st;
+    
+    public Printer(String nome, Storage st)
+    {
+        this.st=st;
+        this.thr=new Thread(this,nome);
+        thr.start();
+    }
+    @Override
+    public void run()
+    {
+        while(true)
+            System.out.println(st.count);
+    }
+}
---
+++ src/ripassothread/Storage.java
@@ -0,0 +1,6 @@
+package ripassothread;
+
+public class Storage 
+{
+    public static int count=0;
+}
